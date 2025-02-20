6acdcf9fe629c806ee2a86a197fb25cb896a4def
gallos <100338941+JramaeGallos@users.noreply.github.com>
2025-02-20 14:39:32 +0800

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,7 +36,7 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand>
+          <Navbar.Brand href="https://jramaegallos.github.io/portfolio/">
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
